ae8ff382472e0158ff85606b5cf26c5a58d6aaae
1470804@qq.com <1470804@qq.com>
2016-06-09 16:19:50 +0800

    更新nuget

--- NuGet/nuget pack.bat
+++ NuGet/nuget pack.bat
@@ -1,8 +1,7 @@
-%cd%\nuget.exe  pack E:\OpenSource\FarseerSolution\Framework\Farseer.Net\Src\Farseer.Net.csproj -Prop Configuration=Release
-%cd%\nuget.exe  pack E:\OpenSource\FarseerSolution\Framework\Farseer.Net.DI\Src\Farseer.Net.DI.csproj -Prop Configuration=Release
-%cd%\nuget.exe  pack E:\OpenSource\FarseerSolution\Framework\Farseer.Net.Redis\Src\Farseer.Net.Redis.csproj -Prop Configuration=Release
-%cd%\nuget.exe  pack E:\OpenSource\FarseerSolution\Framework\Farseer.Net.Sql\Src\Farseer.Net.Sql.csproj -Prop Configuration=Release
-%cd%\nuget.exe  pack E:\OpenSource\FarseerSolution\Framework\Farseer.Net.Utils\Src\Farseer.Net.Utils\Farseer.Net.Utils.csproj -Prop Configuration=Release
-%cd%\nuget.exe  pack E:\OpenSource\FarseerSolution\Framework\Farseer.Net.Utils\Src\Farseer.Net.Utils.Form\Farseer.Net.Utils.Form.csproj -Prop Configuration=Release
-%cd%\nuget.exe  pack E:\OpenSource\FarseerSolution\Framework\Farseer.Net.Utils\Src\Farseer.Net.Utils.Web\Farseer.Net.Utils.Web.csproj -Prop Configuration=Release
+%cd%\nuget.exe  pack E:\Net\FarseerSolution\Framework\Farseer.Net\Src\Farseer.Net.csproj -Prop Configuration=Release
+%cd%\nuget.exe  pack E:\Net\FarseerSolution\Framework\Farseer.Net.Redis\Src\Farseer.Net.Redis.csproj -Prop Configuration=Release
+%cd%\nuget.exe  pack E:\Net\FarseerSolution\Framework\Farseer.Net.Sql\Src\Farseer.Net.Sql.csproj -Prop Configuration=Release
+%cd%\nuget.exe  pack E:\Net\FarseerSolution\Framework\Farseer.Net.Utils\Src\Farseer.Net.Utils\Farseer.Net.Utils.csproj -Prop Configuration=Release
+%cd%\nuget.exe  pack E:\Net\FarseerSolution\Framework\Farseer.Net.Utils\Src\Farseer.Net.Utils.Form\Farseer.Net.Utils.Form.csproj -Prop Configuration=Release
+%cd%\nuget.exe  pack E:\Net\FarseerSolution\Framework\Farseer.Net.Utils\Src\Farseer.Net.Utils.Web\Farseer.Net.Utils.Web.csproj -Prop Configuration=Release
 pause
